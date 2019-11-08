73a58d0c149fb71b7b8208ab3f101bf25e18934c
xingdong <yuanzhi@ruc.edu.cn>
2019-11-08 10:13:33 +0800

    Yuanzhi's Reports

---
+++ 2018104128/src/src/com/zyz/model/PartsInfo.java
@@ -0,0 +1,31 @@
+package com.zyz.model;
+
+public class PartsInfo {
+    private String number;
+    private double price;
+    private int total;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
