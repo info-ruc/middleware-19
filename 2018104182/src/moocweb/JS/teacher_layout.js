b0c1f7724daa17d51a34ab16b7f6f1edcb1ebf9c
beebible <57387445+beebible@users.noreply.github.com>
2019-11-09 13:10:02 +0800

    Add files via upload

---
+++ 2018104182/src/moocweb/JS/teacher_layout.js
@@ -0,0 +1,33 @@
+var data = [{
+    text: '班级管理',
+    iconCls: 'fa fa-users',
+    state: 'close',
+},{
+    text: '题库管理',
+    iconCls: 'fa fa-file-text',
+},{
+    text: '课后练习',
+    iconCls: 'fa fa-pencil',
+    children:[{
+        text:'第一章',
+        iconCls:'fa fa-file-text-o'
+    },{
+        text:'第二章',
+        iconCls:'fa fa-file-text-o'
+    },{
+        text:'第三章',
+        iconCls:'fa fa-file-text-o'
+    }]
+},{
+    text: '考试管理',
+    iconCls: 'fa fa-graduation-cap',
+},{
+    text: '通知管理',
+    iconCls: 'fa fa-comments',
+},{
+    text: '学生反馈',
+    iconCls: 'fa fa-bullhorn',
+},{
+    text: '账号管理',
+    iconCls: 'fa fa-user',
+}, ];
---
+++ 2018104182/src/moocweb/Models/NewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace moocweb.Models {
+    public class TmpLogin {
+        public user_account uaccount { get; set; }
+        public userapply uapply { get; set; }     
+    }
+}
