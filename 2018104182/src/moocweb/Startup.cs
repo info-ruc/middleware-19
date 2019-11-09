5a08d04c8e5ac9b4b7f1d0e0f70d38b332faab5f
beebible <57387445+beebible@users.noreply.github.com>
2019-11-09 13:20:33 +0800

    Add files via upload

---
+++ 2018104182/src/moocweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(moocweb.Startup))]
+namespace moocweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
